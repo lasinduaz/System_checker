31570af0e31c66dd906925d8a60a28fd32e68fe2
lasinduaz <lasinduaz@gmail.com>
2025-12-27 21:39:19 +0530

    Implement get system feature

--- check.py
+++ check.py
@@ -1,3 +1,17 @@
 import platform
 import psutil
 
+def system_info():
+    print("=== SYSTEM INFORMATION ===")
+    print(f"OS        : {platform.system()}")
+    print(f"Machine   : {platform.machine()}")
+
+
+def main():
+    system_info()
+    cpu_check()
+    memory_check()
+    disk_check()
+
+if __name__ == "__main__":
+    main()
